1d7e274b7be9de80571bd0501dc2623a46a83114
TAEWOONG KANG <xodndxnxn@gmail.com>
2023-11-23 18:01:26 +0900

    FIX : prettier version 업그레이드 & 세팅 수정

--- packages/eslint-prettier-config-custom/prettier.base.config.js
+++ packages/eslint-prettier-config-custom/prettier.base.config.js
@@ -1,30 +1,15 @@
-const tailwindPlugin = require('prettier-plugin-tailwindcss');
-const sortImportsPlugin = require('@trivago/prettier-plugin-sort-imports');
-
-const mixedPlugin = {
-  parsers: {
-    typescript: {
-      ...tailwindPlugin.parsers.typescript,
-      preprocess: sortImportsPlugin.parsers.typescript.preprocess,
-    },
-  },
-  options: {
-    ...sortImportsPlugin.options,
-  },
-};
-
-const tsOption = {
-  arrowParens: 'always',
-  jsxSingleQuote: true,
-  bracketSameLine: true,
-  singleQuote: true,
-  semi: true,
-  bracketSpacing: true,
-  useTabs: false,
-  tabWidth: 2,
-  trailingComma: 'all',
-  printWidth: 100,
-  endOfLine: 'auto',
+const option = {
+  arrowParens: 'always', // 화살표 함수 괄호 사용 방식
+  jsxSingleQuote: true, // JSX에 singe 쿼테이션 사용 여부
+  bracketSameLine: true, // JSX의 마지막 `>`를 다음 줄로 내릴지 여부
+  singleQuote: true, // single 쿼테이션 사용 여부
+  semi: true, // 세미콜론 사용 여부
+  bracketSpacing: true, // 객체 리터럴에서 괄호에 공백 삽입 여부
+  useTabs: false, // 탭 사용 여부
+  tabWidth: 2, // 탭 너비
+  trailingComma: 'all', // 여러 줄을 사용할 때, 후행 콤마 사용 방식
+  printWidth: 100, // 줄 바꿈 폭 길이
+  endOfLine: 'auto', // EoF 방식, OS별로 처리 방식이 다름
   importOrder: [
     '^re(.*)$',
     '<THIRD_PARTY_MODULES>',
@@ -33,24 +18,24 @@ const tsOption = {
     '^@(?:assets|constants|hooks|styles|utils|contexts)(.*)$',
     '^[./]',
   ],
-  plugins: [mixedPlugin],
-  importOrderSeparation: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  pluginSearchDirs: false,
   importOrderSortSpecifiers: true,
 };
 
+/** @type {import("prettier").Config} */
 module.exports = {
   overrides: [
     {
-      files: '*.{ts,tsx}',
+      files: '*.{ts,tsx,js,jsx}',
       options: {
-        ...tsOption,
+        ...option,
       },
     },
     {
-      files: '*.{js,jsx}',
+      files: '*.d.ts',
       options: {
-        ...tsOption,
-        printWidth: 80,
+        ...option,
       },
     },
     {
